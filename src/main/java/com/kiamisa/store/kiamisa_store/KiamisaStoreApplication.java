58bc685222238959344d0f0754b862b823ee5fea
Silas Henrique <105889529+Kiamisa@users.noreply.github.com>
2025-07-22 10:57:24 -0300

    Initial commit of Spring Initializr

---
+++ src/main/java/com/kiamisa/store/kiamisa_store/KiamisaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.kiamisa.store.kiamisa_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiamisaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiamisaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiamisa/store/kiamisa_store/KiamisaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiamisa.store.kiamisa_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiamisaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
